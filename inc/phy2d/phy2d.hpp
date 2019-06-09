7bfd140b63ace2604690d1caf737061e07c3fdcf
Ivan Fonseca <ivanfon@riseup.net>
2019-06-09 07:31:12 -0400

    Add include for entire library

---
+++ inc/phy2d/phy2d.hpp
@@ -0,0 +1,39 @@
+/*
+phy2d - A small, simple C++ physics library
+
+Copyright (C) 2019 Ivan Fonseca
+
+phy2d is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+phy2d is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with phy2d.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file phy2d.hpp
+ * @brief phy2d single-include header
+ * @author Ivan Fonseca
+ * @copyright GPL-3.0
+ */
+
+#ifndef INC_PHY2D_PHY2D_HPP_
+#define INC_PHY2D_PHY2D_HPP_
+
+#include "CircleBody.hpp"
+#include "Constants.hpp"
+#include "Forces.hpp"
+#include "RectBody.hpp"
+#include "RigidBody.hpp"
+#include "Utils.hpp"
+#include "Vector.hpp"
+#include "World.hpp"
+
+#endif  // INC_PHY2D_PHY2D_HPP_
